ee8cc766dae2e0ab77daebd8ad233ed8da8efdd0
Gianfranco Manganiello <gianfrancomanganiello@cecotec.es>
2021-09-05 11:40:28 +0200

    Improve cart

--- packages/shared/src/components/CartList/CartList.tsx
+++ packages/shared/src/components/CartList/CartList.tsx
@@ -1,30 +1,12 @@
 import React from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import Colors from '../../constants/Colors'
 import IProduct from '../../types/IPoduct'
+import ProductInfo from './ProductInfo'
 
 interface IProps {
   products: Array<IProduct>
   goToPayPage: () => void
-}
-
-const ProductInfo = (props: { product: IProduct }) => {
-  const { product } = props
-
-  return (
-    <View>
-      <Image source={{ uri: product.image }} />
-
-      <Text>{product.title}</Text>
-
-      <Text>
-        <Text>Precio: </Text> {product.price}
-      </Text>
-
-      <Text>
-        <Text>Cantidad: </Text> 1
-      </Text>
-    </View>
-  )
 }
 
 const CartList = (props: IProps) => {
@@ -32,20 +14,44 @@ const CartList = (props: IProps) => {
   const totalToPay = products.reduce((acc, product) => acc + product.price, 0)
 
   return (
-    <View>
-      {products.map((product) => (
-        <ProductInfo key={product.id.toString()} product={product} />
-      ))}
+    <View style={styles.container}>
+      <View style={styles.productList}>
+        {products.map((product) => (
+          <ProductInfo key={product.id.toString()} product={product} />
+        ))}
+      </View>
 
-      <Text>
-        Total a pagar: <Text>{totalToPay}</Text>
+      <Text style={styles.totalToPay}>
+        Total a pagar: $<Text>{totalToPay}</Text>
       </Text>
 
-      <TouchableOpacity onPress={goToPayPage}>
-        <Text>Pagar</Text>
+      <TouchableOpacity style={styles.payButton} onPress={goToPayPage}>
+        <Text style={styles.payButtonText}>Pagar</Text>
       </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  productList: {},
+  totalToPay: {
+    fontSize: 24,
+  },
+  payButton: {
+    backgroundColor: Colors.CeladonBlue,
+    borderRadius: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginVertical: 15,
+    maxWidth: 280,
+  },
+  payButtonText: {
+    color: Colors.White,
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+})
 
 export default CartList
---
+++ packages/shared/src/components/CartList/ProductInfo.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+import Colors from '../../constants/Colors'
+import IProduct from '../../types/IPoduct'
+
+interface IProps {
+  product: IProduct
+}
+
+const ProductInfo = (props: IProps) => {
+  const { product } = props
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: product.image }} />
+
+      <View>
+        <Text style={styles.title}>{product.title}</Text>
+
+        <Text>
+          <Text style={styles.attributeTitle}>Precio: </Text> {product.price}
+        </Text>
+
+        <Text>
+          <Text style={styles.attributeTitle}>Cantidad: </Text> 1
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+    borderBottomWidth: 5,
+    borderColor: Colors.MintCream,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+    marginRight: 15,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  attributeTitle: {
+    fontWeight: 'bold',
+  },
+})
+
+export default ProductInfo
